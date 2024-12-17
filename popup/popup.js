35aed65fb2cad48307d3c8fde482dcee40ede319
Fnessa <90143741+Fnessa@users.noreply.github.com>
2024-12-17 17:46:18 +0100

    changes

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        let activeTabUrl = tabs[0].url;
+        let activeTabUrl = tabs[0].url;  
         document.getElementById("url").innerHTML = activeTabUrl
         
+        if (activeTabUrl.contains("google.com") == true){
+            getElementById("googlecheck").innerHTML = ":( er google"
+        }
+        if (activeTabUrl.contains("facebook.com") == true){
+            getElementById("facebookcheck").innerHTML = ":( er facebook"
+        }
     })
 });
