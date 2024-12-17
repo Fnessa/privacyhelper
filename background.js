2da2b81710d538934d2f194cf1d3e78dddfc1853
Fnessa <90143741+Fnessa@users.noreply.github.com>
2024-12-17 22:57:14 +0100

    progress

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
-chrome.webNavigation.onCompleted.addListener((details) => {
-  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-    let activeTabUrl = tabs[0].url;  
-  })
-  console.log(activeTabUrl)
-}, {url: [{urlMatches:'https://*/*'}]});
+// Callback function to handle the event
+function getActiveTabUrl(details) {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const activeTabUrl = tabs[0].url;
+    console.log(activeTabUrl);
+  });
+}
+
+// Adding the listener to the webNavigation.onCompleted event
+chrome.webNavigation.onCompleted.addListener(getActiveTabUrl, { url: [{ urlMatches: 'https://*/*' }] });
--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
+let data
+document.addEventListener('DOMContentLoaded', onDOMContentLoaded()); //adds event listener that connects onDOMContentLoaded() to the event DOMContentLoaded
+
+function onDOMContentLoaded(){ 
+    //Fetching json from files
+    data = fetch(chrome.runtime.getURL('data/facebook.com.json'))
+        .then
+    //parsing json
+    data = JSON.parse(data)
+    
+    //Getting currently active tabs.
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        let activeTabUrl = tabs[0].url;  
-        document.getElementById("url").innerHTML = activeTabUrl
-        
+        let activeTabUrl = tabs[0].url;  //Setting activeTabUrl to the url of the current tab
+        document.getElementById("url").innerHTML = activeTabUrl //setting the innerHTML of the element with url id to the activeTabUrl
         if (activeTabUrl.contains("google.com") == true){
             getElementById("googlecheck").innerHTML = ":( er google"
         }
@@ -10,4 +19,4 @@ document.addEventListener('DOMContentLoaded', () => {
             getElementById("facebookcheck").innerHTML = ":( er facebook"
         }
     })
-});
+}
