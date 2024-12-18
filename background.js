362c0eb2b39a0d33970d73bde46fb033e66668d3
Fnessa <90143741+Fnessa@users.noreply.github.com>
2024-12-19 00:34:52 +0100

    prototype function done

--- background.js
+++ background.js
@@ -1,9 +1,21 @@
+let url
 // Callback function to handle the event
 function getActiveTabUrl(details) {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const activeTabUrl = tabs[0].url;
+    url = new URL(activeTabUrl) 
+    if (url.hostname.includes("google.com")){
+      chrome.action.setIcon({ path: "icons/noprivacy-48.png" });
+    }
+    else if (url.hostname.includes("duckduckgo.com")){
+      chrome.action.setIcon({ path: "icons/privacy-48.png" });
+    }
+    else{
+      chrome.action.setIcon({ path: "icons/neutral-48.png" });
+    }
   });
 }
 
 // Adding the listener to the webNavigation.onCompleted event
-chrome.webNavigation.onCompleted.addListener(getActiveTabUrl, { url: [{ urlMatches: 'https://*/*' }] });
+chrome.tabs.onActivated.addListener(getActiveTabUrl);
+
--- popup/popup.js
+++ popup/popup.js
@@ -2,33 +2,31 @@ let data
 document.addEventListener('DOMContentLoaded', onDOMContentLoaded); //adds event listener that connects onDOMContentLoaded() to the event DOMContentLoaded
 
 function onDOMContentLoaded(){ 
-    //Fetching json from files
-    data = fetch(chrome.runtime.getURL('data/google.com.json'))
-    .then(changeContent)
-    .catch(error => {
-        console.error('Error fetching or parsing JSON:', error);
-    });
+   changeContent();
 }
 
 function changeContent(){
-    //parsing json
-    data = JSON.parse(data)
     console.log("AAA");
     //Getting currently active tabs.
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
         let activeTabUrl = tabs[0].url;  //Setting activeTabUrl to the url of the current tab
-        document.getElementById("url").innerHTML = activeTabUrl //setting the innerHTML of the element with url id to the activeTabUrl
-        if (activeTabUrl.includes("google.com") == true){
-            document.getElementById("googlecheck").innerHTML = "☹️ er google"
+        let cleanURL = new URL(activeTabUrl)
+        document.getElementById("url").innerHTML = cleanURL.hostname //setting the innerHTML of the element with url id to the activeTabUrl
+        
+        if (cleanURL.hostname.includes("google.com")){
+            document.getElementById("data").innerHTML = "❌ Blandt andet gemmer Google søgehistorik, IP-adresse, enhedsegenskaber, købshistorik, aktivitet på trejdspartswebsites der andvender google tjenester, og muligvis enhedens sensordata."
+            document.getElementById("time").innerHTML = "❌ Data opbevares i ubegrænset eller uspecificeret længde."
+            document.getElementById("icon").src = "img/RØD LOGO.png"
+        }
+        else if (cleanURL.hostname.includes("duckduckgo.com")){
+            document.getElementById("data").innerHTML = "❕ Ip adresse, browser type, sprog, skærmstørrelse, operativsystem og browser præferencer bruges kortvarig."
+            document.getElementById("time").innerHTML = "✅ Ingen IP eller unikke identifikatorer opbevares af DuckDuckGo"
+            document.getElementById("icon").src = "img/GRØN LOGO.png"
         }
         else{
-            document.getElementById("googlecheck").innerHTML = "☺️ er ikke google"
-        }
-        if (activeTabUrl.includes("facebook.com") == true){
-            document.getElementById("facebookcheck").innerHTML = "☹️ er facebook"
-        }
-        else{
-            document.getElementById("facebookcheck").innerHTML = "☺️ er ikke facebook"
+            document.getElementById("data").innerHTML = "Denne hjemmeside er ikke blevet evalueret."
+            document.getElementById("time").innerHTML = ""
+            document.getElementById("icon").src = "img/GRÅ LOGO.png"
         }
     })
 }
